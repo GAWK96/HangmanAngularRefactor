5eced4e166481f6367a499171d06df3458ac48a1
GAWK96 <guilherme.krieger@yahoo.com.br>
2024-11-23 23:57:30 -0300

    app component ts file changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,17 +14,3 @@ import { AppRoutingModule } from './app.routes';
 export class AppComponent {
   title = 'hangman-game';
 }
-
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterModule, RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [CommonModule, RouterOutlet, RouterModule],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.scss'
-// })
-// export class AppComponent {
-// }
